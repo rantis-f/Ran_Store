cf536d13b0c14cdae93cf0c73867cbdc33e73af3
rantis-f <fayyadrantisismktibazma@gmail.com>
2025-12-12 10:15:25 +0700

    Menambahkan fitur hapus toko

--- website-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ website-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { AlertModal } from "@/components/modals/alert.modal"
 import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Heading } from "@/components/ui/heading"
@@ -54,8 +55,29 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
         }
     }
 
+    const onDelete = async () => {
+        try {
+            setLoading(true)
+            await axios.delete(`/api/stores/${params.storeId}`)
+            router.refresh()
+            router.push('/')
+            toast.success("Toko berhasil dihapus")
+        } catch (error) {
+            toast.error("Cek kembali data dan koneksi mu")
+        } finally {
+            setLoading(false)
+            setOpen(false)
+        }
+    }
+
     return (
         <>
+            <AlertModal
+                isOpen={open}
+                onClose={() => setOpen(false)}
+                onConfirm={onDelete}
+                loading={loading}
+            />
             <div className="flex items-center justify-between">
                 <Heading
                     title="Settings"
--- website-admin/app/api/stores/[storeId]/route.ts
+++ website-admin/app/api/stores/[storeId]/route.ts
@@ -1,16 +1,17 @@
 import db from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { error } from "console";
 import { NextResponse } from "next/server";
 
 export async function PATCH(
     req: Request,
-    { params }: { params: { storeId: string } }
+    { params }: { params: Promise<{ storeId: string }> }
 ) {
     try {
         const { userId } = await auth()
         
         const { name } = await req.json()
+
+        const { storeId } = await params;
 
         if (!userId) {
             return new NextResponse("Unauthenticated", { status: 401 })
@@ -20,13 +21,13 @@ export async function PATCH(
             return new NextResponse("Harus menginput Nama", { status: 400 })
         }
 
-        if (!params) {
+        if (!storeId) {
             return new NextResponse("Store id dibutuhkan", { status: 400 })
         }
 
         const store = await db.store.updateMany({
             where: {
-                id: params.storeId,
+                id: storeId,
                 userId
             },
 
@@ -37,7 +38,7 @@ export async function PATCH(
 
         return NextResponse.json(store)
 
-    } catch {
+    } catch (error) {
         console.log('[STORE_PATCH]', error)
         return new NextResponse("Internal error", { status: 500 })
     }
@@ -45,10 +46,12 @@ export async function PATCH(
 
 export async function DELETE(
     req: Request,
-    { params }: { params: { storeId: string } }
+    { params }: { params: Promise<{ storeId: string }> }
 ) {
     try {
         const { userId } = await auth()
+
+        const { storeId } = await params;
 
         if (!userId) {
             return new NextResponse("Unauthenticated", { status: 401 })
@@ -60,14 +63,14 @@ export async function DELETE(
 
         const store = await db.store.deleteMany({
             where: {
-                id: params.storeId,
+                id: storeId,
                 userId
             },
         })
 
         return NextResponse.json(store)
 
-    } catch {
+    } catch (error) {
         console.log('[STORE_DELETE]', error)
         return new NextResponse("Internal error", { status: 500 })
     }
