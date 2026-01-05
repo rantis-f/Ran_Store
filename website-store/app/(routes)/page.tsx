56c9a62074fc12b8eed5b558ca5259766c163572
rantis-f <fayyadrantisismktibazma@gmail.com>
2026-01-05 17:05:44 +0700

    Environment public api (toko)

--- website-store/app/(routes)/page.tsx
+++ website-store/app/(routes)/page.tsx
@@ -1,5 +1,19 @@
-const HomePage = () => {
-    return ( <div>Home Page</div> );
+import getBanner from "@/actions/get-banner";
+import Banner from "@/components/banner";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0
+
+const HomePage = async () => {
+    const banner = await getBanner('69a4eb65-0f6f-448f-b93a-043dc4b54f1e')
+
+    return (
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Banner data={banner} />
+            </div>
+        </Container>
+    );
 }
  
 export default HomePage;
--- website-store/components/navbar.tsx
+++ website-store/components/navbar.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 import Container from "./ui/container";
 import MainNav from "./main-nav";
+import getCategories from "@/actions/get-categories";
 
-const Navbar = () => {
+export const revalidate = 0
+
+const Navbar = async () => {
+    const categories = await getCategories()
+
     return (
         <div className="border-b">
             <Container>
@@ -10,7 +15,7 @@ const Navbar = () => {
                     <Link href="/" className="ml-4 flex lg:ml-8 gap-x-2">
                         <p className="font-bold text-xl">TOKO</p>
                     </Link>
-                    <MainNav data={[]}/>
+                    <MainNav data={categories} />
                 </div>
             </Container>
         </div>
