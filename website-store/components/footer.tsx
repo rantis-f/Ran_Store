ec5d9ef772bf7961a3e5bb3b06e491237ae1c371
rantis-f <fayyadrantisismktibazma@gmail.com>
2026-01-05 15:54:22 +0700

    Inisiasi website-store dan membuat frontend (toko)

---
+++ website-store/components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-xs text-black">&copy; 2026 RanStore, inc. All right reserved</p>
+            </div>
+        </footer>
+    );  
+}
+
+export default Footer;
