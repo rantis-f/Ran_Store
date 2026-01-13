d35d96b162d8af0a92d9ed81b5e283bc47553804
rantis-f <fayyadrantisismktibazma@gmail.com>
2026-01-13 14:44:23 +0700

    Membuat preview produk (toko)

--- website-store/app/layout.tsx
+++ website-store/app/layout.tsx
@@ -3,6 +3,7 @@ import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+import ModalProvider from "@/providers/modal-provider";
 
 const font = Urbanist({
   subsets: ["latin"],
@@ -21,9 +22,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${font.className}`}
-      >
+      <body className={`${font.className}`}>
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
